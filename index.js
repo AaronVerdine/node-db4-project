2593052a1aa34544d63d2392eb23579adbdea4cd
Your name <a.m.verdine@gmail.com>
2020-05-03 17:28:06 -0400

    Install dependecies/Create index.js/Initialize server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const server = express();
+const port = process.env.PORT || 4000;
+
+server.use(express.json);
+
+server.listen(port, () => {
+  console.log(`Server initalized at http://localhost:${port}`);
+});
