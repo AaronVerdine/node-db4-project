f1d99bca74dbfc5e8498a135133ba9ef77862404
Your name <a.m.verdine@gmail.com>
2020-05-03 17:40:33 -0400

    Update package.json/Sucessfully route to Welcome rotue

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require("express");
 const server = express();
 const port = process.env.PORT || 4000;
 
-server.use(express.json);
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).send(`<h1>Aaron Verdine's Recipe Book</h1>`);
+});
 
 server.listen(port, () => {
   console.log(`Server initalized at http://localhost:${port}`);
